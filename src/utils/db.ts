cf6035c29fbeb52c806a9995c67d7d3dcc46a438
SeaPandaCodes <seapandaramen@gmail.com>
2023-07-04 21:37:34 -0400

    utilize default pg env names

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,10 +2,5 @@ import "dotenv/config";
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: parseInt(process.env.PG_PORT!),
   max: 20, // maximum number of connections in the pool
 });
