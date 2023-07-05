17c69a7ceb6f51f6e95be2b345ceb3fe36b0b150
SeaPandaCodes <seapandaramen@gmail.com>
2023-07-04 20:46:22 -0400

    db connection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import theme from "../utils/theme";
 
-import { Box, ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { trpc } from "@/utils/trpc";
 import { Suspense } from "react";
 import Head from "next/head";
